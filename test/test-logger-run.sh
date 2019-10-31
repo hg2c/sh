dac45fd15ad9cce8257f5a01a6731214e1c3c276
Luo Tao <lotreal@gmail.com>
2019-10-31 15:43:32 +0800

    fix logger:run

--- test/test-logger-run.sh
+++ test/test-logger-run.sh
@@ -13,3 +13,6 @@ foo() {
 foo "-Xms -Xmx" ok.
 
 logger:run foo \"-Xms -Xmx\" ok.
+logger:run /opt/findbugs-3.0.1/bin/findbugs -jvmArgs \"-Xms128m -Xmx2048m\" -home /opt/findbugs-3.0.1 \
+           -textui -effort:max -sortByClass -html -output findbug-result.html \
+           ~/Entropy/lotreal/findbugs-demo/build/classes
